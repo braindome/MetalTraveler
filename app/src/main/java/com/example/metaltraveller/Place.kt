2291051515a9d9a10a494462aa4917fd4de4ef23
Antonio <antonio.saracino83@gmail.com>
2022-12-22 19:22:37 +0100

    Rating bar implemented.

--- app/src/main/java/com/example/metaltraveller/Place.kt
+++ app/src/main/java/com/example/metaltraveller/Place.kt
@@ -8,13 +8,13 @@ import com.google.firebase.firestore.DocumentId
 // NOTE Each argument could be null so remember ? and null in the declaration.
 
 data class Place(
-    var name : String? = "",
-    var rating : Int? = 0,
-    var type : String? = "",
-    var position : MyLatLng? = MyLatLng(0.0, 0.0),
-    var location : String? = "",
-    var favorite : Boolean = false,
-    var expandable : Boolean = false,
+    var name: String? = "",
+    var rating: Float? = null,
+    var type: String? = "",
+    var position: MyLatLng? = MyLatLng(0.0, 0.0),
+    var location: String? = "",
+    var favorite: Boolean = false,
+    var expandable: Boolean = false,
     @DocumentId
-    var documentId : String? = "") {
+    var documentId: String? = "") {
 }
