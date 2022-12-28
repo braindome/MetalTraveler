8958421e7a4f2d961762dce29815e1e0d3990471
Antonio <antonio.saracino83@gmail.com>
2022-12-28 15:14:23 +0100

    Libraries and dependencies added.

--- app/src/main/java/com/example/metaltraveller/DataManager.kt
+++ app/src/main/java/com/example/metaltraveller/DataManager.kt
@@ -10,4 +10,7 @@ object DataManager {
     val places = mutableListOf<Place>()
     val favorites = mutableListOf<Place>()
 
+    //Documentation image galleries:
+    //https://developer.android.com/codelabs/basic-android-kotlin-training-internet-images#2
+
 }
