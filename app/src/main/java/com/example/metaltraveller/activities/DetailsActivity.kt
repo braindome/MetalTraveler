02ef5925b90593c481032632deab3440a11d12ae
Antonio Saracino <antonio.saracino83@gmail.com>
2022-12-30 04:26:09 +0100

    Pictures are now uploaded to database. TODO: Add them as field in the doccuments

--- app/src/main/java/com/example/metaltraveller/activities/DetailsActivity.kt
+++ app/src/main/java/com/example/metaltraveller/activities/DetailsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.metaltraveller.activities
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
@@ -10,6 +12,7 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.metaltraveller.R
@@ -17,11 +20,15 @@ import com.example.metaltraveller.databinding.ActivityMainBinding
 import com.example.metaltraveller.utils.Utils
 import com.google.android.gms.location.*
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.firebase.storage.FirebaseStorage
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -32,7 +39,9 @@ class DetailsActivity : AppCompatActivity() {
     lateinit var imageUrls : ArrayList<String>
 
     lateinit var browseButton : Button
+    lateinit var uploadButton : Button
     lateinit var itemPhoto : ImageView
+    lateinit var imageUri : Uri
 
     companion object {
         val IMAGE_REQUEST_CODE = 1_000;
@@ -51,10 +60,15 @@ class DetailsActivity : AppCompatActivity() {
         val back : Button = findViewById(R.id.backButton)
         setSupportActionBar(findViewById(R.id.detailsToolbar))
         browseButton = findViewById(R.id.browseButton)
+        uploadButton = findViewById(R.id.uploadButton)
         itemPhoto = findViewById(R.id.itemPhoto)
 
         browseButton.setOnClickListener {
             pickImageFromGallery()
+        }
+
+        uploadButton.setOnClickListener {
+            uploadImage()
         }
 
 
@@ -92,16 +106,41 @@ class DetailsActivity : AppCompatActivity() {
         }
     }
 
+    private fun uploadImage() {
+        val progressDialog = ProgressDialog(this)
+        progressDialog.setMessage("Uploading...")
+        progressDialog.setCancelable(false)
+        progressDialog.show()
+
+        val formatter = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault())
+        val now = Date()
+        val fileName = formatter.format(now)
+        val storageRef = FirebaseStorage.getInstance().getReference("images/$fileName")
+
+        storageRef.putFile(imageUri)
+            .addOnSuccessListener {
+                itemPhoto.setImageURI(null)
+                Toast.makeText(this@DetailsActivity, "Successfully uploaded", Toast.LENGTH_SHORT).show()
+                if (progressDialog.isShowing) progressDialog.dismiss()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this@DetailsActivity, "Failed to upload", Toast.LENGTH_SHORT).show()
+                if (progressDialog.isShowing) progressDialog.dismiss()
+            }
+    }
+
     private fun pickImageFromGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
+        val intent = Intent()
         intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, 100)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
-            itemPhoto.setImageURI(data?.data)
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            imageUri = data?.data!!
+            itemPhoto.setImageURI(imageUri)
         }
     }
 
